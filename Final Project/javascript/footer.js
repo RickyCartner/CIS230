df913b38866b59a55344ec47765e2de45cc39311
Ricky Cartner <rcartner002@sc.rr.com>
2021-02-28 02:32:52 -0500

    Final upload

---
+++ Final Project/javascript/footer.js
@@ -0,0 +1,6 @@
+
+function changeFooter() {
+    document.getElementById("footer-div").innerHTML = "Columbia College &#8226; 1301 Columbia College Dr &#8226; Columbia, SC<br>Ricky Cartner &#8226; CIS 230<br>Copyright <sup>&copy;</sup> 2021 ITBS Archer";
+}
+
+window.addEventListener('load', changeFooter, false);
